eb349796496c4500dd0d294b454ddf293755d56c
anasmoaya <45191108+anasmoaya@users.noreply.github.com>
2024-01-21 12:05:17 +0100

    Delete TaxRateService directory

--- TaxRateService/src/main/java/com/tnb/taxrateservice/repositories/TaxRateRepository.java
+++
@@ -1,9 +0,0 @@
-package com.tnb.taxrateservice.repositories;
-
-import com.tnb.taxrateservice.entities.TaxRate;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.Optional;
-
-public interface TaxRateRepository extends JpaRepository<TaxRate, String> {
-}
